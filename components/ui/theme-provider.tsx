c678b7acb2f7387864673a99896895d53d18dd43
Iqra Kamran <iqrakamran147@gmail.com>
2025-07-07 21:40:22 +0500

    Fix ThemeProvider props for deployment

--- components/ui/theme-provider.tsx
+++ components/ui/theme-provider.tsx
@@ -1,12 +1,10 @@
-'use client'
+"use client";
 
-import { ThemeProvider as NextThemesProvider } from 'next-themes'
-import type { PropsWithChildren } from 'react'
+import * as React from "react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 
-export function ThemeProvider({ children }: PropsWithChildren) {
-  return (
-    <NextThemesProvider attribute="class" defaultTheme="system" enableSystem>
-      {children}
-    </NextThemesProvider>
-  )
+type ThemeProviderProps = React.ComponentProps<typeof NextThemesProvider>;
+
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
